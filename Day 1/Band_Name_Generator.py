8d85b06422c2200e5205b2f51ebe9e8f537e1fb6
Nader Mamdouh <nadermamdouh20@gmail.com>
2023-10-02 19:06:11 +0300

    Band_Name_Generator.py

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to band name Geneyrator\n")
+City = input("What is the city you grew up in :\n")
+Pet = input("What is the fav animal you love :\n")
+print("Your Band Name is  "+City+" "+Pet)
