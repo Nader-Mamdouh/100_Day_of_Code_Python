169f279c245e7de966f879c7b04e067744660651
Nader Mamdouh <nadermamdouh20@gmail.com>
2023-10-03 18:22:55 +0300

    Create Your life in weeks.py

---
+++ Day 2/Your life in weeks.py
@@ -0,0 +1,7 @@
+# Day 2.3
+# f-string
+age = int(input("What is your current age ? "))
+months = age*12
+weeks = age*52
+days = age*365
+print(f"You lived {months} month , and {weeks} week and {days} day")
