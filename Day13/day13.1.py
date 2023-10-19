b6acc55aab4929d55c33f3bca3852dcf5c5ebd51
Nader Mamdouh <nadermamdouh20@gmail.com>
2023-10-19 17:17:22 +0300

    Create day13.1.py

---
+++ Day13/day13.1.py
@@ -0,0 +1,5 @@
+num = int(input("What is the number to check even or odd ?"))
+if num % 2 == 0:  # it should be "==" not "="
+    print("The number is EVEN ")
+else:
+    print("The number is ODD ")
