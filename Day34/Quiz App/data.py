913635a533ec3a77e0f904eb57b3575aae05bee1
Nader Mamdouh <nadermamdouh20@gmail.com>
2023-11-19 19:06:08 +0200

    Update data.py

--- Day34/Quiz App/data.py
+++ Day34/Quiz App/data.py
@@ -1,4 +1,4 @@
 import requests as api
-response = api.get(url="https://opentdb.com/api.php?amount=10&category=18&type=boolean")
+response = api.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
 api_data = response.json()
 question_data = api_data["results"]
